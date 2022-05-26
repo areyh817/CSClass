a580ecb7870e07dbc5ab581fbc8cbc7eadad3bbf
Hyera Cho <s2037@e-mirim.hs.kr>
2022-05-26 11:47:30 +0900

    #6 클래스 리스트 연습 리스트에서 항목 제거할 때 인덱스 변경 주의

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -33,7 +33,7 @@ namespace CSClass
             list.Add(303);
             */
 
-            List<int> list = new List<int> {52, 273, 32, 24};
+            List<int> list = new List<int> { 52, 273, 32, 24 };
             list.Remove(52);
 
 
@@ -58,8 +58,56 @@ namespace CSClass
 
             // 인스턴스 변수를 생성과 동시에 초기화(C# 고유 문법)
             Product productB = new Product() { name = "소금빵", price = 2000 };
-            Product productC = new Product() { price = 5500, name = "당근케이크"};
+            Product productC = new Product() { price = 5500, name = "당근케이크" };
             Product productD = new Product() { name = "보름달" };
+
+
+            /*
+
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { name = "조혜라", grade = 2 });
+            students.Add(new Student() { name = "이민지", grade = 1 });
+            students.Add(new Student() { name = "양지나", grade = 1 });
+            students.Add(new Student() { name = "홍의서", grade = 3 });
+            students.Add(new Student() { name = "김민서", grade = 4 });
+            students.Add(new Student() { name = "김유리", grade = 2 });
+            students.Add(new Student() { name = "오민경", grade = 6 });
+
+            */
+
+            List<Student> students = new List<Student>()
+            {
+             new Student() { name = "조혜라", grade = 2 },
+             new Student() { name = "이민지", grade = 1 },
+             new Student() { name = "양지나", grade = 1 },
+             new Student() { name = "홍의서", grade = 3 },
+             new Student() { name = "김민서", grade = 4 },
+             new Student() { name = "김유리", grade = 2 },
+             new Student() { name = "오민경", grade = 6 },
+        };
+
+            /*
+
+            foreach (var item in students)
+            {
+                if(item.grade > 2)
+                {
+                    students.Remove(item);
+                }
+
+                Console.WriteLine(item);
+            }
+
+            */
+
+            for(int i = students.Count - 1; i >= 0; i--)
+            {
+                if(students[i].grade > 2)
+                {
+                    students.RemoveAt(i);
+                }
+            }
+
 
         }
 
